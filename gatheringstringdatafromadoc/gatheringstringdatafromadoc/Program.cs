dd6b043dfd227504b43e819d307bba78ed30a8b8
Blankpanda <Blankpanda8@gmail.com>
2015-05-25 16:57:38 -0400

    started reader
    need to read .DOC files or .DOCX files downloaded from google API
    
    working on the reader on .DOC/.DOCX files
    
    currently need:
    ZIP extractor (if using .DOCX) files

---
+++ gatheringstringdatafromadoc/gatheringstringdatafromadoc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gatheringstringdatafromadoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"Testing.docx";
+            changeEXTtoTxt(path);
+            path = @"Testing.txt";
+            string info = readTxtFile(path);
+
+            Console.WriteLine(info);
+            Console.ReadLine();
+        }
+
+        private static string readTxtFile(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            string info = reader.ReadToEnd();
+            reader.Close();
+            return info;
+
+        }
+
+        private static void changeEXTtoTxt(string path)
+        {
+            File.Move(path, Path.ChangeExtension(path, ".zip"));       
+        }
+    }
+}
