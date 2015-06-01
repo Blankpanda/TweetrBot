4c238e5e2b4439c3eea733eba50947414a787593
Blankpanda <Blankpanda8@gmail.com>
2015-06-01 13:47:56 -0400

    parsing xml
    started writing parser for xml
    found way to dynamically get files

--- gatheringstringdatafromadoc/gatheringstringdatafromadoc/Program.cs
+++ gatheringstringdatafromadoc/gatheringstringdatafromadoc/Program.cs
@@ -4,34 +4,107 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO.Compression;
+using System.Xml;
+using System.Xml.Resolvers;
 namespace gatheringstringdatafromadoc
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string path = @"Testing.docx";
-            changeEXTtoTxt(path);
-            path = @"Testing.txt";
-            string info = readTxtFile(path);
-
-            Console.WriteLine(info);
-            Console.ReadLine();
+            DirectoryInfo h = new DirectoryInfo(Directory.GetCurrentDirectory());
+            FileInfo[] pong = h.GetFiles();
+            Console.WriteLine(pong[pong.Length - 1].Extension);
+           
+           //changeFileExtension(@"Testing.zip", ".docx"); // we need to convert this docx file to a ZIP.
+           //extractor(@"Testing.zip", "Testing"); // extract the newly created zip is nessecary
+           //string readData = readXmlData(); // read the XML data in the zip file (from converting a docx file to a zip)
+           
+           //Console.WriteLine(readData);
+           Console.ReadLine();
         }
 
+        private static dynamic readXmlData()
+        {
+            string r = readTxtFile(@"Testing\word\document.xml");
+            StringBuilder output = new StringBuilder();
+
+            using( XmlReader reader = XmlReader.Create( new StringReader(r) ) ) 
+            {
+                reader.ReadToFollowing("w:r");
+                reader.MoveToFirstAttribute();
+                string word = reader.Value;
+
+                output.AppendLine(word);
+
+                return output.ToString();
+            }
+
+        }
+
+        private static string writeTxtFile(string path)
+        {
+            try
+            {
+
+                StreamWriter w = new StreamWriter(path);
+               /* place XML data in a data structure and write to this */
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Couldn't write to  File!");
+                throw;
+               
+            }
+
+            return null;
+        }
         private static string readTxtFile(string path)
         {
-            StreamReader reader = new StreamReader(path);
-            string info = reader.ReadToEnd();
-            reader.Close();
-            return info;
+          /*read the wirtten file if nesecary */
+            try
+            {
+                StreamReader reader = new StreamReader(path);
+                string info = reader.ReadToEnd();
+                reader.Close();
+                return info;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("File not found ( or file in the wrong format )");
+                return string.Empty;
+            }
+     
 
         }
 
-        private static void changeEXTtoTxt(string path)
+        private static void extractor(string zipPath , string newPath)
         {
-            File.Move(path, Path.ChangeExtension(path, ".zip"));       
+            try
+            {
+                ZipFile.ExtractToDirectory(zipPath, newPath);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("File already Extracted, moving along");
+               
+            }
+            
+        }
+        private static void changeFileExtension(string path, string newType)
+        {
+            try
+            {
+                File.Move(path, Path.ChangeExtension(path, newType));       
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("File has already been changed to the proper format");
+               
+            }
+         
         }
     }
 }
