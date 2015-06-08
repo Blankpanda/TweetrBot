b8bd78d46699b2a1799272b9bac00799066be283
Blankpanda <Blankpanda8@gmail.com>
2015-06-08 18:32:50 -0400

    splitting text into seperate text files

--- gatheringstringdatafromadoc/gatheringstringdatafromadoc/Program.cs
+++ gatheringstringdatafromadoc/gatheringstringdatafromadoc/Program.cs
@@ -21,7 +21,7 @@ namespace gatheringstringdatafromadoc
              extractor(path, "XMLdata");                // extract the newly created zip if nessecary.
              string output = readXmlData();            // read the XML data in the zip file (from converting a docx file to a zip).
              writeTxtFile("parsedXML.txt", output);   // write raw output to a text file.
-             formatLunchDoc("parsedXML.txt");
+             formatLunchDoc("parsedXML.txt");        // format the text files and move data around for easier access of information in node.js
              
            Console.WriteLine();
            Console.ReadLine();
@@ -29,7 +29,8 @@ namespace gatheringstringdatafromadoc
 
         #region --lunch DOC--
 
-
+        /* a lot of different formating that involves seperating text into different files 
+           and removing all unneeded text */
         private static void formatLunchDoc(string content)
         {
             List<string> contentList = new List<string>();
@@ -91,27 +92,16 @@ namespace gatheringstringdatafromadoc
                 catch (Exception)
                 {
                     sb.Clear();
-                     break;   
+                    break;   
                     
                 }
                  
             }
 
             // we need to specifically remove the footing information from the friday textfile.
-            char[] cDayData = dayData[4].ToCharArray();
-            // gets the length of the array up to a certain character
-            int subStrCounter = 0;
-            for (int i = 0; i < cDayData.Length - 1 ; i++)
-            {
-                if (cDayData[i] != '*')
-                {
-                    subStrCounter++;
-                }
-                else
-                {
-                    break;
-                }
-            }
+            int subStrCounter = 
+                SeperateString(dayData[4], '*');
+           
             // assigns a substring of DayData[4] to DayData[4]
             dayData[4] = dayData[4].Substring(0, subStrCounter);
 
@@ -127,12 +117,35 @@ namespace gatheringstringdatafromadoc
                writeTxtFile(@"Days\" + DaysOfTheWeek[i] + ".txt", dayData[i]);
             }
 
-            
+
+            /*
+             * 
+             * due to the nature of tweets, text files will be seperated into a lunch menu and a sandwich menu
+             * It will be like this:
+             * MONDAY -> MONDAY_LUNCH | MONDAY_SANDWICH
+             * 
+             */
+
+            string currentFileContent = string.Empty;
+            subStrCounter = 0;
+            for (int i = 0; i < DaysOfTheWeek.Count; i++)
+            {
+                currentFileContent = readTxtFile( @"Days\" + DaysOfTheWeek[i] + ".txt" );
+                subStrCounter = SeperateString( currentFileContent, "SANDWICH CHOICE" );
+                currentFileContent = currentFileContent.Substring(0, subStrCounter);
+                writeTxtFile(@"Days\" + DaysOfTheWeek[i] + "_LUNCH" + ".txt", currentFileContent);
+            }
+
+      
 
         }
+
+        /*mainly because we need to remove the elements and then use
+        it again after we've removed the elements and it would be
+        abstract pain to type it out again */
         private static List<string> initalizeDaysOfTheWeekList(List<string> DaysOfTheWeek)
         {
-            //mainily because we need to remove the elements and then use it again after we've removed the elements
+            
             DaysOfTheWeek.Clear();
             DaysOfTheWeek.Add("MONDAY");
             DaysOfTheWeek.Add("TUESDAY");
@@ -144,9 +157,11 @@ namespace gatheringstringdatafromadoc
 
         }
 
+
+        //Checks a list to see if the first element is empty
        private static bool isEmptyList(List<string> l )
         {
-            if (l.Contains(string.Empty))
+            if (l[0].Contains(string.Empty))
             {
                 return true;
             }
@@ -157,145 +172,224 @@ namespace gatheringstringdatafromadoc
 
         }
 
-        #endregion
-        
-
-        private static string getFilesInDirectory(string type)
+  
+        // generates how long different substrings will be based on a specfic condition
+       private static int SeperateString(string str , char condition)
         {
+            int counter = 0;
+            char[] c = str.ToCharArray();
 
-            //populate an array of type FileInfo with the paths of the items in the working directory
-            DirectoryInfo d = new DirectoryInfo(Directory.GetCurrentDirectory());
-            FileInfo[] files = d.GetFiles();
 
-            /* convert files array into a string array to check for .DOCX file and have a string return type and gets the file
-               path of the file that contains .docx then returns it to main 
-               we also need to make sure zip files make it through so when 
-               only an .zip file exists we return that as well */
-            string path = string.Empty;
-
-            for (int i = 0; i < files.Length; i++)
+            for (int i = 0; i <= c.Length; i++)
             {
-                if (files[i].ToString().Contains(type))
+                if  (c[i] != condition) 
                 {
-                    
-                    path = files[i].FullName.ToString();
+                    counter++;
+                }
+                else
+                {
                     break;
                 }
             }
-
-            return path;
-
-        }
-        private static dynamic readXmlData()
-        {
-
-            string docPath = @"XMLdata\word\document.xml";
-            StringBuilder output = new StringBuilder();
-
-            // parse XML elements and builds a string based on the reader
-            using( XmlReader reader = XmlReader.Create( docPath ) ) 
-            {
-
-                while ( reader.Read() )
-                {
-                    if (reader.HasValue == true)
-                    {
-                        output.AppendLine(reader.Value);
-                    }
-                }
-
-
-            }
-
-            return output.ToString(); 
-
-        }
-        private static void writeTxtFile(string name , string content)
-        {
-            string pathCheck = getFilesInDirectory(".txt"); //check to see if the working directory already contains a extractedXML.txt file
-            try
-            {
-
-                content = content.Replace("version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"", String.Empty);
-
-
-                if (pathCheck.Contains(name))
-                { Console.WriteLine("parsedXML.txt already exists (nothing to write)"); }
-
-                else
-                {         
-                      StreamWriter w = new StreamWriter(name);
-                    
-                      w.WriteLine(content);
-                      w.Close();
-                }
-
-
-
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Couldn't write to  File!");
-                throw;
-               
-            }
-
-            
-
-        }
-        private static string readTxtFile(string path)
-        {
-
-            /*read the wirtten file if nesecary */
-            try
-            {
-                StreamReader reader = new StreamReader(path);
-                string info = reader.ReadToEnd();
-                reader.Close();
-                return info;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("File not found ( or file in the wrong format )");
-                return string.Empty;
-            }
-
-        }
-        private static void extractor(string zipPath , string newPath)
-        {
-
-            try
-            {
-                string checkPath = getFilesInDirectory(".zip");
-                if ( checkPath.Contains("XMLdata") )
-                { Console.WriteLine("File already extracted, moving along"); }
-                else
-                {
-                    ZipFile.ExtractToDirectory(zipPath, newPath);
-                }
-                       
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("File already Extracted, moving along");
-               
-            }
-            
-        }
-        private static void changeFileExtension(string path, string newType)
-        {
-
-            try
-            {
-                File.Move(path, Path.ChangeExtension(path, newType));       
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("File has already been changed to the proper format");
-               
-            }
-
+            return counter;
         }
 
+
+        // overload method for SeperateString that evaluates the condition as a string and not a char
+       private static int SeperateString(string str, string condition)
+       {
+           int accum = 0;
+           string[] words = str.Split(' ');
+          
+
+           for (int i = 0; i < words.Length; i++)
+           {
+               string breakConditon = words[i] + " " + words[i + 1];
+               breakConditon = RemoveBreakChars(breakConditon);
+
+               if ( breakConditon != condition )
+               {
+                   accum += words[i].Length;
+               }
+               else
+               {
+                   accum += words[i].Length;
+                   break;
+               }
+           }
+
+           return accum;
+       }
+
+       private static string RemoveBreakChars(string s)
+       {
+           char[] c = s.ToCharArray();
+           s = string.Empty;
+           for (int i = 0; i < c.Length; i++)
+           {
+               if (!(c[i] == '\r' || c[i] == '\n'))
+               {
+                   s += c[i];
+               }
+           }
+
+           return s;
+       }
+
+
+
+
+
+        #endregion
+
+
+       #region --File Manipulation--
+
+
+       // gets a the specific path of a file in a directory when there is only one of those files
+       private static string getFilesInDirectory(string type)
+       {
+
+           //populate an array of type FileInfo with the paths of the items in the working directory
+           DirectoryInfo d = new DirectoryInfo(Directory.GetCurrentDirectory());
+           FileInfo[] files = d.GetFiles();
+
+           /* convert files array into a string array to check for .DOCX file and have a string return type and gets the file
+              path of the file that contains .docx then returns it to main 
+              we also need to make sure zip files make it through so when 
+              only an .zip file exists we return that as well */
+           string path = string.Empty;
+
+           for (int i = 0; i < files.Length; i++)
+           {
+               if (files[i].ToString().Contains(type))
+               {
+
+                   path = files[i].FullName.ToString();
+                   break;
+               }
+           }
+
+           return path;
+
+       }
+        //read the XML from the converted document
+       private static dynamic readXmlData()
+       {
+
+           string docPath = @"XMLdata\word\document.xml";
+           StringBuilder output = new StringBuilder();
+
+           // parse XML elements and builds a string based on the reader
+           using (XmlReader reader = XmlReader.Create(docPath))
+           {
+
+               while (reader.Read())
+               {
+                   if (reader.HasValue == true)
+                   {
+                       output.AppendLine(reader.Value);
+                   }
+               }
+
+
+           }
+
+           return output.ToString();
+
+       }
+        // write to a text file
+       private static void writeTxtFile(string name, string content)
+       {
+           string pathCheck = getFilesInDirectory(".txt"); //check to see if the working directory already contains a extractedXML.txt file
+           try
+           {
+
+               content = content.Replace("version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"", String.Empty);
+
+
+               if (pathCheck.Contains(name))
+               { Console.WriteLine("parsedXML.txt already exists (nothing to write)"); }
+
+               else
+               {
+                   StreamWriter w = new StreamWriter(name);
+
+                   w.WriteLine(content);
+                   w.Close();
+               }
+
+
+
+           }
+           catch (Exception)
+           {
+               Console.WriteLine("Couldn't write to  File!");
+               throw;
+
+           }
+
+
+
+       }
+        //read a text file
+       private static string readTxtFile(string path)
+       {
+
+           /*read the wirtten file if nesecary */
+           try
+           {
+               StreamReader reader = new StreamReader(path);
+               string info = reader.ReadToEnd();
+               reader.Close();
+               return info;
+           }
+           catch (Exception)
+           {
+               Console.WriteLine("File not found ( or file in the wrong format )");
+               return string.Empty;
+           }
+
+       }
+        //extract the generated ZIP file
+       private static void extractor(string zipPath, string newPath)
+       {
+
+           try
+           {
+               string checkPath = getFilesInDirectory(".zip");
+               if (checkPath.Contains("XMLdata"))
+               { Console.WriteLine("File already extracted, moving along"); }
+               else
+               {
+                   ZipFile.ExtractToDirectory(zipPath, newPath);
+               }
+
+           }
+           catch (Exception)
+           {
+               Console.WriteLine("File already Extracted, moving along");
+
+           }
+
+       }
+        //convert the file from file.whatever to file.somethingElse
+       private static void changeFileExtension(string path, string newType)
+       {
+
+           try
+           {
+               File.Move(path, Path.ChangeExtension(path, newType));
+           }
+           catch (Exception)
+           {
+               Console.WriteLine("File has already been changed to the proper format");
+
+           }
+
+       }
+
+       #endregion
+    
     }
 }
